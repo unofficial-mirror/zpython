4aeb7b4b0e5732f55909c9bc86c5690b495d5822
ZyX <kp-pav@yandex.ru>
2014-12-08 01:00:55 +0300

    Add OrderedDict implementation from python-2.7
    There is no such a thing in python-2.6, so tests are failing there.

--- test/ztstutil.py
+++ test/ztstutil.py
@@ -58,6 +58,153 @@ class CArray(Array):
     def __call__(self, a):
         self.accesses+=['set:'+'|'.join((s(i) for i in a))]
 
+try:
+    from collections import OrderedDict
+except ImportError:
+    # From http://hg.python.org/cpython/rev/026ee0057e2d3305f90a9da41daf7c3f9eb1e814
+    #
+    # Stripped off documentation and comments
+    class OrderedDict(dict):
+        def __init__(self, *args, **kwds):
+            if len(args) > 1:
+                raise TypeError('expected at most 1 arguments, got %d' % len(args))
+            try:
+                self.__root
+            except AttributeError:
+                self.__root = root = []
+                root[:] = [root, root, None]
+                self.__map = {}
+            self.__update(*args, **kwds)
+
+        def __setitem__(self, key, value, dict_setitem=dict.__setitem__):
+            if key not in self:
+                root = self.__root
+                last = root[0]
+                last[1] = root[0] = self.__map[key] = [last, root, key]
+            return dict_setitem(self, key, value)
+
+        def __delitem__(self, key, dict_delitem=dict.__delitem__):
+            dict_delitem(self, key)
+            link_prev, link_next, _ = self.__map.pop(key)
+            link_prev[1] = link_next
+            link_next[0] = link_prev
+
+        def __iter__(self):
+            root = self.__root
+            curr = root[1]
+            while curr is not root:
+                yield curr[2]
+                curr = curr[1]
+
+        def __reversed__(self):
+            root = self.__root
+            curr = root[0]
+            while curr is not root:
+                yield curr[2]
+                curr = curr[0]
+
+        def clear(self):
+            root = self.__root
+            root[:] = [root, root, None]
+            self.__map.clear()
+            dict.clear(self)
+
+        def keys(self):
+            return list(self)
+
+        def values(self):
+            return [self[key] for key in self]
+
+        def items(self):
+            return [(key, self[key]) for key in self]
+
+        def iterkeys(self):
+            return iter(self)
+
+        def itervalues(self):
+            for k in self:
+                yield self[k]
+
+        def iteritems(self):
+            for k in self:
+                yield (k, self[k])
+
+        update = MutableMapping.update
+
+        __update = update
+
+        __marker = object()
+
+        def pop(self, key, default=__marker):
+            if key in self:
+                result = self[key]
+                del self[key]
+                return result
+            if default is self.__marker:
+                raise KeyError(key)
+            return default
+
+        def setdefault(self, key, default=None):
+            if key in self:
+                return self[key]
+            self[key] = default
+            return default
+
+        def popitem(self, last=True):
+            if not self:
+                raise KeyError('dictionary is empty')
+            key = next(reversed(self) if last else iter(self))
+            value = self.pop(key)
+            return key, value
+
+        def __repr__(self, _repr_running={}):
+            call_key = id(self), _get_ident()
+            if call_key in _repr_running:
+                return '...'
+            _repr_running[call_key] = 1
+            try:
+                if not self:
+                    return '%s()' % (self.__class__.__name__,)
+                return '%s(%r)' % (self.__class__.__name__, self.items())
+            finally:
+                del _repr_running[call_key]
+
+        def __reduce__(self):
+            items = [[k, self[k]] for k in self]
+            inst_dict = vars(self).copy()
+            for k in vars(OrderedDict()):
+                inst_dict.pop(k, None)
+            if inst_dict:
+                return (self.__class__, (items,), inst_dict)
+            return self.__class__, (items,)
+
+        def copy(self):
+            return self.__class__(self)
+
+        @classmethod
+        def fromkeys(cls, iterable, value=None):
+            self = cls()
+            for key in iterable:
+                self[key] = value
+            return self
+
+        def __eq__(self, other):
+            if isinstance(other, OrderedDict):
+                return dict.__eq__(self, other) and all(_imap(_eq, self, other))
+            return dict.__eq__(self, other)
+
+        def __ne__(self, other):
+            return not self == other
+
+        def viewkeys(self):
+            return KeysView(self)
+
+        def viewvalues(self):
+            return ValuesView(self)
+
+        def viewitems(self):
+            return ItemsView(self)
+
 class Hash(object):
     def accappend(self, a):
         if self.acc and self.acc[-1][0] == a:
@@ -66,7 +213,6 @@ class Hash(object):
             self.acc.append([a, 1])
 
     def __init__(self):
-        from collections import OrderedDict
         self.d = {'a': 'b'} if sys.version_info < (3,) else {b'a': b'b'}
         self.d = OrderedDict(self.d)
         self.acc = []
